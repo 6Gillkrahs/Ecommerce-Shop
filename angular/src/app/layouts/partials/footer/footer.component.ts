502c87be2117b1d3395e6f3f7a642442839ca361
6Gillkrahs <dvq2804@gmail.com>
2024-05-18 18:38:17 +0700

    update-layout

--- angular/src/app/layouts/partials/footer/footer.component.ts
+++ angular/src/app/layouts/partials/footer/footer.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
 
+  ngOnInit(): void {
+    
+  }
 }
